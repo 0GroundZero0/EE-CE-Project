998dd44f335724878f19b99a8b2c95c90210e51c
0GroundZero0 <70299566+0GroundZero0@users.noreply.github.com>
2021-12-03 08:04:48 -0500

    BRuh

--- FinalProject.vhd
+++ FinalProject.vhd
@@ -3,8 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity FinalProject is
 	port(
-		clock, reset, X1, X2 : IN STD_LOGIC;
-		A, B : OUT STD_LOGIC_VECTOR (6 downto 0)
+		clock, reset, X1, X2, fan : IN STD_LOGIC;
+		A, B : OUT STD_LOGIC_VECTOR (6 downto 0);
+		C : OUT STD_LOGIC
 	);
 end FinalProject;
 
@@ -17,13 +18,18 @@ component CounterDisplay
     );
 end component;
 
-signal R1, R2, Rin : STD_LOGIC;
+signal NX1, NX2, R1, R2, Rin : STD_LOGIC;
 
 begin
 
-u1: CounterDisplay port map (clock, Rin, X1, A(6), A(5), A(4), A(3), A(2), A(1), A(0), R1);
-u2: CounterDisplay port map (clock, Rin, X2, B(6), B(5), B(4), B(3), B(2), B(1), B(0), R2);
+NX1 <= not X1;
+NX2 <= not X2;
 
-Rin <= reset or r1 or r2;
+C <= fan;
+
+u1: CounterDisplay port map (clock, Rin, NX1, A(6), A(5), A(4), A(3), A(2), A(1), A(0), R1);
+u2: CounterDisplay port map (clock, Rin, NX2, B(6), B(5), B(4), B(3), B(2), B(1), B(0), R2);
+
+Rin <= not reset or r1 or r2;
 
 end arch;
